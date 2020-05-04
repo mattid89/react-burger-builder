a1d1498b7c4018a5181797bb3b92d06a3ce1adfc
Matias Doto <matydoto@gmail.com>
2020-05-03 21:48:03 -0300

    mensaje cuandno no hay indgredientes. flatmap para aplanar el array de ingredientes

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,13 +3,17 @@ import classes from './Burger.module.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
-    const transformIngredients = Object.keys(props.ingredients)
-        .map(igKey => {
+    let transformIngredients = Object.keys(props.ingredients)
+        .flatMap(igKey => {
             return [...Array(props.ingredients[igKey])]
                 .map((_, i) => {
                     return <BurgerIngredient key={igKey + i} type={igKey} />
                 });
         });
+    if (transformIngredients.length === 0) {
+        transformIngredients = <p>Please add some ingredients!</p>;
+    }
+        console.log(transformIngredients)
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top" />
